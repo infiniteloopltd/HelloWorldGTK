b70331f054d3e69406fc76a296feb14121a1e683
fiach <fiach.reid@gmail.com>
2018-11-25 13:20:40 +0000

    Initial Commit

---
+++ HelloWorldGTK/MainWindow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+
+        WebClient wc = new WebClient();
+        var strIP = wc.DownloadString("http://icanhazip.com");
+        label2.Text = strIP;
+
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
